ad92c60a839826570a4d7a899e8792415bb68274
zhaoyibin <zhaoyibin@le.com>
2016-08-11 11:32:21 +0800

    second commit

---
+++ src/main/java/com/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.hello;
+
+/**
+ * Created by Nico on 8/11/16 11:13.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
